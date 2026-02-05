d6c861972fe6fa635f2f63966e97dd2840ec4ffa
Carlos2BB <cedu97978@gmail.com>
2026-02-04 23:20:17 -0300

    enemies.js

--- enemies.js
+++ enemies.js
@@ -1,6 +1,6 @@
 let enemies = [
-    { x: 600, y: 432, vida: 30 },
-    { x: 900, y: 432, vida: 30 }
+    { x: 600, y: 396, vida: 40 },
+    { x: 900, y: 396, vida: 40 }
 ];
 
 function updateEnemies(player) {
@@ -8,5 +8,11 @@ function updateEnemies(player) {
         if (Math.abs(player.x - e.x) < 200) {
             e.x += player.x > e.x ? 1 : -1;
         }
+
+        if (Math.abs(player.x - e.x) < 40) {
+            player.vida -= 0.3;
+        }
     });
+
+    enemies = enemies.filter(e => e.vida > 0);
 }
